ef84c449c0e93381c8a66023828e66dfe4e5b846
armaulan <armaulan@gmail.com>
2024-03-09 21:18:51 +0700

    Fix: Route Monitoring/dms-data-alerting.php

--- public/index.php
+++ public/index.php
@@ -14,7 +14,7 @@ $routes = array(
     '/dist-last-trans' => '../src/Cmd/Dist/dist-last-trans.php',
     '/agent-chatbot-order' => '../src/Cmd/Agent/agent-chatbot-order.php',
     '/telegram-rtpdm' => '../src/Cmd/Webhook/telegram-rtpdm.php',
-    '/dms-data-alerting' => '../src/Cmd/Monitoring/dms-data-qc.php',
+    '/dms-data-alerting' => '../src/Cmd/Monitoring/dms-data-alerting.php',
     '/dms-data-monitoring' => '../src/Cmd/Monitoring/dms-data-monitoring.php',
     '/dms-data-query' => '../src/Cmd/Monitoring/dms-data-query-download.php',
 
